5100c9fc83a079dfd600a9255f9b4e4e5a608f3b
Sebastian <lookeper2@gmail.com>
2023-03-19 17:13:10 +0100

    think i fixed the use of msnodesqlv8

--- src/utils/useQueryHandler.js
+++ src/utils/useQueryHandler.js
@@ -1,11 +1,29 @@
-import { SqlClint } from "msnodesqlv8";
+const sql = require("msnodesqlv8");
 
+const connectionString = "server=.;Database=Master;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT name FROM sys.databases";
 
-export const useQueryHandler = async (query) =>
-  SqlClint.query(process.env.REACT_APP_CONNECTION_STRING, query, (error, response) => {
-    if (error) {
-      console.error(err);
-      return;
-    }
-    return response;
+sql.query(connectionString, query, (err, rows) => {
+    console.log(rows);
+});
+
+function useQueryHandler(query) {
+  return new Promise((resolve, reject) => {
+    sql.open(config, (err, conn) => {
+      if (err) {
+        reject(err);
+      } else {
+        conn.query(query, (error, results) => {
+          conn.close();
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
+        });
+      }
+    });
   });
+}
+
+module.exports = { useQueryHandler };
